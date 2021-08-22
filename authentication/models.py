e498ea8c6b4011f820d00b42b80909961be27304
Pankaja Suganda <papsperera0@gmail.com>
2021-08-23 00:59:57 +0530

    base model completed

--- authentication/models.py
+++ authentication/models.py
@@ -16,9 +16,46 @@ JOB_ROLES = [
     ('staff','STAFF')
 ]
 
+class CustomAccountManager(BaseUserManager):
+    def create_user(self, user_name, password):
+        
+
+        if not password:
+            raise ValueError(_("You Must Provide Pasword"))
+
+        user = self.model(user_name = user_name)
+
+        # if Group.objects.filter(name = user.role).exists():
+        #     group = Group.objects.filter(name = user.role)
+        #     user.groups.add(group)
+        user.is_staff = True
+        user.is_active = True
+        user.is_superuser = True 
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    
+    def create_superuser(self, user_name, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('Superuser Must Be Assigned to is_staff = True')
+
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser Must Be Assigned to is_superuser = True')
+
+        return self.create_user( user_name, password)
+
 class BaseUser(AbstractBaseUser, PermissionsMixin):
 
+    email = models.EmailField('email address', blank=True)
     user_name = models.CharField('user name', max_length=150, unique=True)
+    first_name = models.CharField('first name', max_length=150, blank=True)
+    last_name = models.CharField('last name', max_length=150, blank=True)
     mobile_number = models.CharField(max_length=10, blank=True)
     Address_1 = models.CharField('address 1', max_length=250, blank=True)
     Address_2 = models.CharField('address 2', max_length=250, blank=True)
@@ -27,9 +64,11 @@ class BaseUser(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
 
-    objects = UserManager()
+    objects = CustomAccountManager()
 
     USERNAME_FIELD = 'user_name'
+    REQUIRED_FIELDS = ['email']
+
     class meta:
         proxy = True
     def __str__(self):
