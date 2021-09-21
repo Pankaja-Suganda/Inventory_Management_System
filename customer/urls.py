f08165c2ff238594a32b7a0e8027b07cb451c9e1
Pankaja-Suganda <69086445+Pankaja-Suganda@users.noreply.github.com>
2021-09-22 01:09:18 +0530

    finished for today

--- customer/urls.py
+++ customer/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from views import customer_info
 
 urlpatterns = [
-    path('customer_info/', customer_info, name="customer_info")
+    path('customer_info/<str:id>', customer_info, name="customer_info")
 ]
