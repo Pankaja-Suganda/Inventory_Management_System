3e8030833a7b6c586e19863d23827cd2938d1ed8
Pankaja-Suganda <69086445+Pankaja-Suganda@users.noreply.github.com>
2021-09-23 04:40:18 +0530

    made a tabke click customer and supplier cange function

--- customer/urls.py
+++ customer/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from views import customer_info
+from .views import customer_info
 
 urlpatterns = [
     path('customer_info/<str:id>', customer_info, name="customer_info")
